8100ae6e5fa5aaa2679a44ec6e9e159d719d8e00
anugerahdwi <anugrahdwi941@gmail.com>
2023-01-10 18:11:26 +0700

    Update component header #1

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { Menu } from '../../interfaces/menu';
+
+import { MenuService } from '../../services/menu/menu.service';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+	constructor(private menuService: MenuService) { }
 
+	// Data Menu
+	allMenu: Menu[] = [];
+
+	ngOnInit(): void {
+		// mengambil data menu header
+		this.dataMenuHeader();
+	}
+
+	// Get Data Menu Header
+	// mengambil semua data menu header dari database
+	dataMenuHeader() {
+		this.menuService.getDataMenuPosisi('header').subscribe(res => {
+			this.allMenu = res;
+		});
+	}
 }
